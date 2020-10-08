e478ba3c49d136186a1a1568a9c5b5fa8cf89d7b
Roman Kovayin <r.kovaykin@avsw.ru>
2020-10-08 14:47:23 +0300

    Initial commit

---
+++ SberTestTask/NewsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SberTestTask
+//
+//  Created by Роман Ковайкин on 08.10.2020.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
